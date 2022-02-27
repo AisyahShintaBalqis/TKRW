e71a787a21f0c6a04902ed78421c2980ce20a59c
AisyahShintaBalqis <shintabalqis30301@gmail.com>
2022-02-27 17:16:40 +0700

    H78

--- forminput.php
+++ forminput.php
@@ -2,36 +2,57 @@
 <html>
 <head>
 	<title>Form Input Mahasiswa</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
 
-	<h1>Form Input Mahasiswa</h1>
+	
 
-	<form action="{{route('store-lecturer')}}" method="POST">
+	<div class="col-md-6 mx-auto mt-5 container"> 
+      <div class="card">
+        <div class="card-header bg-primary text-white">Form Input Data Mahasiswa</div>
+        <div class="card-body">
+
+	<form action="proses-simpan.php" method="POST">
+
+		<div class="mb-3">
+			<label for="exampleFormControlNim" class="form-label">NIM</label>
+			<input type="text" class="form-control" name="Nim" id="exampleFormControlInput1" placeholder="nim">
+		</div>
+
+		<div class="mb-3">
+			<label for="exampleFormControlNama" class="form-label">NAMA</label>
+			<input type="text" class="form-control" name="Nama" id="exampleFormControlNama" placeholder="Nama">
+		</div>
+
+<div class="mb-3">
+	<label for="exampleFormControljk" class="form-label">Jenis Kelamin</label>
+		<select class="form-select" name="Jenis_Kelamin" aria-label="Default select example">
+			
+			<option value="Laki-Laki">Laki-Laki</option>
+			<option value="Perempuan">Perempuan</option>
+		</select>
+	</div>
+
+	<div class="mb-3">
+			<label for="exampleFormControlangkatan" class="form-label">Angkatan</label>
+			<input type="number" class="form-control" name="Angkatan" id="exampleFormControlankatan" placeholder="Angkatan">
+		</div>
+
+		<button type="submit" class="btn btn-primary">Submit</button>
 
 
-		<label>NIM</label>
-		<br>                 
-		<input type="text" name="nIM"  placeholder="NIM">
-		<br><br>
 
-		<label>NAMA</label> 
-		<br>                
-		<input type="text" name="nama"  placeholder="NAMA">
-		<br><br>
 
-		<label >Jenis Kelamin</label>
-		<br>                 
-		<input type="text" name="jk"  placeholder="Jenis Kelamin">
-		<br><br>
-
-		<label for="exampleInputNidn" class="form-label">Angkatan</label> 
-		<br>                 
-		<input type="number" name="anhkatan"  placeholder="Angkatan">
-		<br><br><br>                 
-
-<button type="submit">Simpan</button>
+		
 	</form>
+</div>
+</div>
+</div>
+
 
 </body>
 </html>
---
+++ proses-simpan.php
@@ -0,0 +1,21 @@
+<?php
+
+$Nim = $_POST['Nim'];
+$Nama = $_POST['Nama'];
+$Jenis_Kelamin = $_POST['Jenis_Kelamin'];
+$Angkatan = $_POST['Angkatan'];
+
+include 'koneksi.php';
+$koneksi = mysqli_connect('localhost', 'root', '', 'kampus');
+
+$sql="INSERT INTO mahasiswa (Nim, Nama, Jenis_Kelamin, Angkatan) VALUES ('$Nim ', '$Nama', '$Jenis_Kelamin', '$Angkatan')";
+
+if (mysqli_query($koneksi, $sql)) {
+	echo "Data Berhasil Di Simpan";
+	
+}
+else{
+	echo "Data gagal di simpan" . $sql . "<br>" . mysqli_error($koneksi) ;
+}
+
+?>
