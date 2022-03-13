6d1738753caed2d3e383b3087047b7009eb502fc
AisyahShintaBalqis <shintabalqis30301@gmail.com>
2022-03-13 18:58:26 +0700

    H92

--- perpustakaan/index.php
+++ perpustakaan/index.php
@@ -2,10 +2,31 @@
 <html>
 <head>
 	<title>Perpustakaan</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
+	<div class="col-md-10 mx-auto mt-5 container">
+		<div class="card">
+			<div class="card-header bg-primary text-white">Beranda</div>		
 
-	<h1>Sistem Informasi Perpustakaan</h1>
+			<div class="card-body">
+				<div class="col-md-2 text-right">
+					<a class="btn btn-success  btn-sm" href="tampilbuku.php"> Data Buku </a>
+				</div>
+
+				<h1>Sistem Informasi Perpustakaan</h1>
+
+				<p>Sistem Informasi Manajemen Perpustakaan (e-library) merupakan sistem informasi yang dikembangkan untuk memudahkan dalam pelayanan serta memudahkan petugas perpustakaan dalam mengelola perpustakaan. Petugas perpustakaan dapat selalu memonitor tentang ketersediaan buku, daftar buku baru, peminjaman buku dan pengembalian buku.</p>
+
+				<p>Dengan sistem ini, peminjam buku maupun yang mengembalikan buku tidak perlu menunggu lama untuk proses peminjaman/pengembalian buku. Petugas perpustakaan pun tidak akan mengalami kesulitan dalam proses pelaporan kepada kepala perpustakaan. Sistem informasi perpustakaan yang berbasis webbase, memudahkan kita untuk mengakses perpustakaan online, bahkan mengetahui rekam jejak aktifitas pengunjung perpustakaan.</p>
+
+			</div>
+		</div>
+	</div>
+
 
 </body>
 </html>
