4fcc721a162272b80ce5c0636aa8ae55c4fea2fd
AisyahShintaBalqis <shintabalqis30301@gmail.com>
2022-03-17 18:35:52 +0700

    H96

--- perpustakaan/edit.php
+++ perpustakaan/edit.php
@@ -24,6 +24,12 @@ if (mysqli_num_rows($result) > 0) {
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+	<style>
+		body {
+			background-color: lightblue;
+		}
+	</style>
+
 </head>
 <body>
 
--- perpustakaan/forminput.php
+++ perpustakaan/forminput.php
@@ -5,6 +5,12 @@
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	<style>
+		body {
+			background-color: lightblue;
+		}
+	</style>
 </head>
 <body>
 	<div class="col-md-5 mx-auto mt-5 container">
