8e880943778c12b4767e0173640a455c78a9674b
AisyahShintaBalqis <shintabalqis30301@gmail.com>
2022-03-20 19:17:51 +0700

    H99

--- shop/index.php
+++ shop/index.php
@@ -74,15 +74,15 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+          <span>Belanja</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <a class="collapse-item" href="utilities-color.html">Pilih Barang</a>
+            <a class="collapse-item" href="utilities-border.html">Pesan</a>
+            <a class="collapse-item" href="utilities-animation.html">Checkout</a>
+            <a class="collapse-item" href="utilities-other.html">Konfirmasi</a>
           </div>
         </div>
       </li>
